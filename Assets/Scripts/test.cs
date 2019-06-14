3e3ceb8f19f1b750106f041596c00ac0cf162ac8
Unknown <ernham1@gmail.com>
2019-06-14 15:26:57 +0900

    csv파싱 테스트를 위한 파서 추가

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class test : MonoBehaviour {
+
+	public int _exp = 0;
+
+	void Start () {
+		List<Dictionary<string,object>> data = CSVReader.Read("GdCandyInfo");
+
+		for(var i=0; i< data.Count; i++){
+			Debug.Log("id" + (i).ToString() + " : " + data[i]["name"] + " " + data[i]["prefabs"] + " " + data[i]["dropRate"]+" " + data[i]["Speed"]);
+		}
+
+		_exp = (int)data[0]["id"];
+		Debug.Log("id");
+     
+	}
+}
